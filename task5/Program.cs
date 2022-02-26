217590e56928475ad17399a99bafc82b76564aac
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 02:35:04 +0300

    done

---
+++ task5/Program.cs
@@ -0,0 +1,19 @@
+﻿int max(int a, int b, int c)
+{
+    int Max = a > b ? a : b;
+    Max = Max > c ? Max : c;
+    return Max;
+}
+
+int a, b, c;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+c = Convert.ToInt32(s);
+System.Console.WriteLine(max(a,b,c));
