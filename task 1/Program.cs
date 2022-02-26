447a822a3b442ed984b75b26b341280576507fbe
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 02:07:12 +0300

    done

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool kvadr(int a, int b)
+{
+    if(a==b*b) return true;
+    else return false;
+}
+int a, b;
+string s;
+System.Console.WriteLine("Write a number");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+System.Console.WriteLine("Write a number");
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+if(kvadr(a,b)) System.Console.WriteLine($"{a} является квадратом {b}");
+else System.Console.WriteLine($"{a} не является квадратом {b}");
