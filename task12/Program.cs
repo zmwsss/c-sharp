0e833c0eef3bf1664d4ee01b428ec864e9b354ad
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 02:57:47 +0300

    done

---
+++ task12/Program.cs
@@ -0,0 +1,14 @@
+﻿int maxdigit(int n)
+{
+    int a=n/10;
+    int b=n%10;
+    int max=a>b ? a:b;
+    return max;
+}
+
+int n;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+n = Convert.ToInt32(s);
+System.Console.WriteLine(maxdigit(n));
