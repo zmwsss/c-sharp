74b7b75bac046dce296f76c238840209ac3bcdea
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 02:21:12 +0300

    done

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿bool maxmin(int a, int b)
+{
+    if (a > b) return true;
+    else return false;
+}
+
+int a, b;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+if (maxmin(a, b)) System.Console.Write($"{a}>{b}");
+else System.Console.Write($"{a} < {b}");
