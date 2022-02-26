4c4b8fb74539083ab1cef18e86566b6d92f86c6e
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 02:49:44 +0300

    done

---
+++ task9/Program.cs
@@ -0,0 +1,8 @@
+﻿int n;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+n = Convert.ToInt32(s);
+for(int i=1;i<=n;i++){
+    if(i%2==0) System.Console.Write($"{i} ");
+    }
