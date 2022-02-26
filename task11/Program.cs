e9a54ec5619a6589530e919f49bd97f5cbab637a
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 02:54:30 +0300

    done

---
+++ task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int digit(int n)
+{
+    n/=10;
+    n%=10;
+    return n;
+}
+
+int n;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+n = Convert.ToInt32(s);
+System.Console.WriteLine(digit(n));
