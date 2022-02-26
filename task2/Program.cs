1f9bc83ab4ce7ceb29639a96466f8e8d18d5f75b
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 02:13:18 +0300

    done

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int kvadr(int a)
+{
+    a*=a;
+    return a;
+}
+
+int a;
+string s;
+System.Console.WriteLine("Write a number");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+System.Console.WriteLine(kvadr(a));
