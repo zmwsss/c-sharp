15aac48b368bd8997f3cebccbc860a7308e64adc
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 02:43:29 +0300

    done

---
+++ task7/Program.cs
@@ -0,0 +1,11 @@
+﻿int unoreven(int a)
+{
+    return a%2;
+}
+int a;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+if(unoreven(a)==0) System.Console.WriteLine($"{a} четное");
+else System.Console.WriteLine($"{a} нечетное");
