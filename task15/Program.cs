ac9b739b1b31ca4e028d932db245f1bc5b150200
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 03:13:39 +0300

    done

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿int digit3(int n)
+{
+    n/=100;
+    n%=10;
+    return n;
+}
+
+int n;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+n = Convert.ToInt32(s);
+if(n<100) System.Console.WriteLine("У числа нет третьей цифры");
+System.Console.WriteLine(digit3(n));
