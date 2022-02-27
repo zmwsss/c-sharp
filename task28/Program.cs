50796c26c205c58063b07e8e83ac36493876fd0a
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 15:18:26 +0300

    done

---
+++ task28/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+int a, i;
+string s;
+System.Console.WriteLine("Enter a number");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+for(i=0;a>0;a/=10)
+{
+    i+=a%10;
+}
+System.Console.WriteLine($"The sum of digits number are {i}");
