c79eb5b2d02bddd7ffc6b306d209dea3f570ba45
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 03:02:32 +0300

    done

---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿int deldigit(int n)
+{
+    int a=n/100;
+    int b=n%10;
+    n=a*10+b;
+    return n;
+}
+
+int n;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+n = Convert.ToInt32(s);
+System.Console.WriteLine(deldigit(n));
