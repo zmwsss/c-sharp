3356c29664f83f2379ac4eff1321d62085c4f562
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 15:35:22 +0300

    done

---
+++ task30/Program.cs
@@ -0,0 +1,9 @@
+﻿int n;
+string s;
+System.Console.WriteLine("Enter a number");
+s=Console.ReadLine();
+n=Convert.ToInt32(s);
+for(int i=1; i<=n;i++)
+{
+    if((i*i*i)%2==0) System.Console.Write($"{i*i*i} ");
+}
