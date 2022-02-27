2449a56391d1a7a50fce05bfb7f3084dcc3000a5
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 03:25:02 +0300

    done

---
+++ task16/Program.cs
@@ -0,0 +1,12 @@
+﻿bool weekend(int a)
+{
+    if(a==6 || a==7) return true;
+    else return false;
+}
+int n;
+string s;
+System.Console.WriteLine("Write a day of week");
+s = Console.ReadLine();
+n = Convert.ToInt32(s);
+if(weekend(n)) System.Console.WriteLine("день недели является выходным"); 
+else System.Console.WriteLine("день недели не является выходным");
