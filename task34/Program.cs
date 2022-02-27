32afe11e2aed2f6625764a80f0faa428c53b8d32
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 16:17:59 +0300

    done

---
+++ task34/Program.cs
@@ -0,0 +1,21 @@
+﻿void replace(int[] a)
+{
+    for(int i=0; i<a.Length; i++)
+    {
+        a[i]=-a[i];
+    }
+}
+int[] a=new int[12];
+Random rand = new Random();
+for(int i=0;i<12;i++)
+{
+    a[i]=rand.Next(-9,9);
+    System.Console.Write($"{a[i]} ");
+}
+replace(a);
+System.Console.WriteLine();
+for(int i=0;i<12;i++)
+{
+    System.Console.Write($"{a[i]} ");
+}
+
