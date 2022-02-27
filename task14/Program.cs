cae4fc9d900a5be5a394c5e814b86dd12c176cec
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 03:09:02 +0300

    done

---
+++ task14/Program.cs
@@ -0,0 +1,15 @@
+﻿int div(int a, int b)
+{
+    return a%b;
+}
+
+int a, b;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+if(div(a,b)==0) System.Console.WriteLine($"{a} делится на {b}");
+else System.Console.WriteLine($"{a} делится на {b} с остатком {div(a,b)}");
