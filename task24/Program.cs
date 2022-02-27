0d71f2caac826f1aadfddbc22611d093981fadcb
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 14:03:10 +0300

    done

---
+++ task24/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать кубы чисел от 1 до N
+int n;
+string s;
+System.Console.WriteLine("Enter a number");
+s=Console.ReadLine();
+n=Convert.ToInt32(s);
+for(int i=1;i<=n;i++)
+{
+    System.Console.WriteLine($"the cube of the number {i} is {i*i*i}");
+}
