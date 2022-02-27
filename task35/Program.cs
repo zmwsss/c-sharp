72db7c72e285be24f0683d9fd88695693f2dd5d6
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 16:28:07 +0300

    done

---
+++ task35/Program.cs
@@ -0,0 +1,21 @@
+﻿bool num(int[] a, int b)
+{
+    for(int i=0; i<a.Length; i++)
+    if(a[i]==b) return true;
+    return false;
+}
+int[] a=new int[12];
+int b;
+string s;
+System.Console.WriteLine("enter a number");
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+Random rand = new Random();
+for(int i=0;i<a.Length;i++)
+{
+    a[i]=rand.Next(-9,9);
+    System.Console.Write($"{a[i]} ");
+}
+System.Console.WriteLine();
+if(num(a,b)) System.Console.WriteLine($"{b} is an array");
+else System.Console.WriteLine($"{b} is not an array");
