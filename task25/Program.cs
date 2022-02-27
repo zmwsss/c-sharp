3ae299a2a529b402f2ae4caa7fc00dd0635e04c2
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 14:06:31 +0300

    done

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму чисел от 1 до N
+int n, sum=0;
+string s;
+System.Console.WriteLine("Enter a number");
+s=Console.ReadLine();
+n=Convert.ToInt32(s);
+for(int i=1;i<=n;i++)
+{
+    sum+=i;
+}
+System.Console.WriteLine(sum);
