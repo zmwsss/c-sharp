bebdbb73d2ad1ea6256cd3918b702e2d4774b04c
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 15:27:09 +0300

    done

---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿int fact(int n)
+{
+    int i, factorial=1;
+    for(i=1;i<=n;i++)
+    {
+        factorial*=i;
+    }
+    return factorial;
+}
+int n;
+string s;
+System.Console.WriteLine("Enter a number");
+s=Console.ReadLine();
+n=Convert.ToInt32(s);
+System.Console.WriteLine($"factorial {n} is {fact(n)}");
