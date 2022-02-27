93608b0312d3cb2f4ab84c0c0dac851ccdc0629d
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 03:20:16 +0300

    done

---
+++ task15_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int div(int a)
+{
+    int r1=a%7;
+    int r2=a%23;
+    return r1+r2;
+}
+
+int a;
+string s;
+System.Console.WriteLine("Write a number");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+if(div(a)==0) System.Console.WriteLine($"{a} делится на 7 и 23");
+else System.Console.WriteLine($"{a} не делится на 7 и 23");
