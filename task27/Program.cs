bf096c5c8a6650df970940ee6655ecfdf8b8f97a
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 14:30:20 +0300

    done

---
+++ task27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Определить количество цифр в числе
+int a, i;
+string s;
+System.Console.WriteLine("Enter a number");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+for(i=0;a>0;i++)
+{
+    a/=10;
+}
+System.Console.WriteLine($"number of digits are {i}");
