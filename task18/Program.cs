1b9438b9aab0c9265e55bd958acb271e3aa4dffa
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 04:28:55 +0300

    done

---
+++ task18/Program.cs
@@ -0,0 +1,16 @@
+﻿bool[] X={true, false};
+bool[] Y={true, false};
+bool flag=true;
+for(int i=0; i<2; i++)
+{
+    for(int j=i;j<2;j++)
+    {
+        if(!(X[i] || Y[j])!=(!X[i] && !Y[j]))
+        {
+            flag=false;
+            break;
+        }
+    }
+}
+if(flag) System.Console.WriteLine("the expression is true");
+else System.Console.WriteLine("the expression is false");
