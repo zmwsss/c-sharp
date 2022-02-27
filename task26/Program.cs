f99901aed69c04e9181e539777bc73758fcbb7f7
zmwsss <smirnov2003daniil@mail.ru>
2022-02-27 14:19:42 +0300

    done

---
+++ task26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+int a, b;
+string s;
+System.Console.WriteLine("Enter a number A");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+System.Console.WriteLine("Enter a number B");
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+int t=a;
+for(int i=1;i<b;i++)
+{
+    a*=t;
+    
+}
+System.Console.WriteLine($"A to the power of B is {a}");
+
