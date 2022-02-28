b0d97f390cf1ae7e0f3891db78c0fa540a64cf68
zmwsss <smirnov2003daniil@mail.ru>
2022-02-28 15:39:53 +0300

    done

---
+++ task37/Program.cs
@@ -0,0 +1,16 @@
+﻿ int kolvo(int[] a)
+ {
+     int n=0;
+     for(int i=0; i<a.Length;i++)
+     {
+         if(a[i]>=10 && a[i]<=99) n++;
+     }
+     return n;
+ }
+ int[] a=new int [123];
+ Random rand= new Random();
+ for(int i=0;i<a.Length;i++)
+{
+    a[i]=rand.Next(10,99);
+}
+ System.Console.WriteLine(kolvo(a));
