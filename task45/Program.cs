8e39dd54d590e8f72db6802944c5583bb11f381c
zmwsss <smirnov2003daniil@mail.ru>
2022-02-28 16:48:27 +0300

    done

---
+++ task45/Program.cs
@@ -0,0 +1,21 @@
+﻿void fibonacci(int[] a)
+{
+    a[0]=1;
+    a[1]=1;
+    for(int i=2;i<a.Length;i++)
+    {
+        a[i]=a[i-1] + a[i-2];
+    }
+}
+
+int n;
+string s;
+System.Console.WriteLine("Enter № of fibonacci");
+s=Console.ReadLine();
+n=Convert.ToInt32(s);
+int[] a=new int[n];
+fibonacci(a);
+for(int i=0; i<n; i++)
+{
+    System.Console.Write($"{a[i]} ");
+}
