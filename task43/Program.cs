cde63d6af09136f156f9f48bbb232c12f63200be
zmwsss <smirnov2003daniil@mail.ru>
2022-02-28 16:35:13 +0300

    done

---
+++ task43/Program.cs
@@ -0,0 +1,27 @@
+﻿int bit(int a)
+{
+    int n=0;
+    for(int i=30;i>=0;i--)
+    {
+
+        if(a==(int)Math.Pow(2,i))
+        {
+             n+=(int)Math.Pow(10,i);
+             return n;
+        }
+        if(a>(int)Math.Pow(2,i))
+            {
+             n+=(int)Math.Pow(10,i);
+             a-=(int)Math.Pow(2,i);
+            }
+
+    }
+    return n;
+}
+int a;
+string s;
+System.Console.WriteLine("Enter a positive number");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+System.Console.WriteLine(bit(a));
+
