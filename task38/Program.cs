06045e9604c22209cd0ee1315c6c1be3824b5f1b
zmwsss <smirnov2003daniil@mail.ru>
2022-02-28 23:16:16 +0300

    второй способ

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,19 +1,31 @@
-﻿int SumOdd(int[] a)
-{
-    int sum=0;
-    for(int i=0; i<a.Length; i++)
-    {
-        if(i%2==0) sum+=a[i];
-    }
-    return sum;
-}
+﻿// int SumOdd(int[] a)
+// {
+//     int sum=0;
+//     for(int i=0; i<a.Length; i++)
+//     {
+//         if(i%2==0) sum+=a[i];
+//     }
+//     return sum;
+// }
 
-int[] a=new int[10];
-Random rand = new Random();
-for(int i=0; i<a.Length; i++)
+// int[] a=new int[10];
+// Random rand = new Random();
+// for(int i=0; i<a.Length; i++)
+// {
+//     a[i]= rand.Next(-10, 10);
+//     System.Console.Write($"{a[i]} ");
+// }
+// System.Console.WriteLine();
+// System.Console.WriteLine(SumOdd(a));
+void bitprint(int a)
 {
-    a[i]= rand.Next(-10, 10);
-    System.Console.Write($"{a[i]} ");
+    for(int i=31;i>=0;i--)
+    {
+        if((a&(1<<i))!=0) System.Console.Write(1);
+        else System.Console.Write(0);
+        if(i%8==0) System.Console.Write($" ");
+    }
 }
-System.Console.WriteLine();
-System.Console.WriteLine(SumOdd(a));
+int a=int.Parse(Console.ReadLine());
+System.Console.WriteLine(a);
+bitprint(a);
