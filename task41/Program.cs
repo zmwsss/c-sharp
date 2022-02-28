5c69d9379780f5ee6a2b8c7901dc25f9b9658e06
zmwsss <smirnov2003daniil@mail.ru>
2022-02-28 15:51:04 +0300

    done

---
+++ task41/Program.cs
@@ -0,0 +1,18 @@
+﻿bool triangle(double a, double b, double c)
+{
+    if(a+b>c && a+c>b && b+c>a) return true;
+    return false;
+}
+double a, b ,c;
+string s;
+System.Console.WriteLine("Enter a hand of triangle");
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+System.Console.WriteLine("Enter a hand of triangle");
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+System.Console.WriteLine("Enter a hand of triangle");
+s=Console.ReadLine();
+c=Convert.ToDouble(s);
+if(triangle(a,b,c)) System.Console.WriteLine($"{a} {b} {c} are hands of triangle");
+else System.Console.WriteLine($"{a} {b} {c} are not hands of triangle");
