805f34c93a338659f59e6fecfd336ecb4e2821c1
zmwsss <smirnov2003daniil@mail.ru>
2022-03-01 20:03:16 +0300

    done

---
+++ task74/Program.cs
@@ -0,0 +1,15 @@
+﻿//int n=Convert.ToInt32(Console.ReadLine());
+void Words(string alphabet, char[]word, int length)
+{
+    if(length==word.Length) 
+    {
+        System.Console.Write($"{new String(word)} ");
+        return;
+    }
+    for(int i=0;i<alphabet.Length;i++)
+    {
+        word[length]=alphabet[i];
+        Words(alphabet, word,length+1);
+    }
+}
+Words("аисв", new char[3], 0);
