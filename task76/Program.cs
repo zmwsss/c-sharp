6dc8296719eb49548d619f6eb55e5bad778eff11
zmwsss <100049961+zmwsss@users.noreply.github.com>
2022-03-01 23:12:19 +0300

    Update Program.cs

--- task76/Program.cs
+++ task76/Program.cs
@@ -1,7 +1,7 @@
 ﻿bool combination(string a, string b)
 {
     bool flag;
-    for(int i=0;i<a.Length && a.Length>=b.Length;i++)
+    for(int i=0;i<a.Length;i++)
     {
         flag=false;
         for(int j=0;j<b.Length;j++)
@@ -15,7 +15,7 @@
 }
 System.Console.WriteLine("Введите первое слово");
 string s1=Console.ReadLine();
-System.Console.WriteLine("Введите второе слово, не длиннее второго");
+System.Console.WriteLine("Введите второе слово, не длиннее первого");
 string s2=Console.ReadLine();
 if(combination(s1,s2)) System.Console.WriteLine("Из букв первого слова можно составить второе слово");
 else System.Console.WriteLine("Из букв первого нельзя слова можно составить второе слово");
