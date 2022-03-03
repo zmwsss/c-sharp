d31c586effaa2d76534654af6adda6a27b326756
zmwsss <smirnov2003daniil@mail.ru>
2022-03-03 15:56:33 +0300

    done

---
+++ task42/Program.cs
@@ -0,0 +1,12 @@
+﻿int numbers(double[] a)
+{
+    int k=0;
+    for(int i=0; i<a.Length; i++)
+    {
+        if(a[i]>0) k++;
+    }
+    return k;
+}
+System.Console.WriteLine("Введите числа:");
+double[] a=Console.ReadLine().Split(' ').Select(e => Convert.ToDouble(e)).ToArray();
+System.Console.WriteLine($"Вы ввели {numbers(a)} чисел больше нуля");
