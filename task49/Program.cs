92f5ce3b806ad14c9fec61cf1d5ddfbc30a2d5f7
zmwsss <smirnov2003daniil@mail.ru>
2022-03-03 16:37:34 +0300

    done

---
+++ task49/Program.cs
@@ -0,0 +1,19 @@
+﻿void Input(int m, int n, double[,] a)
+{
+    Random rand=new Random();
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            a[i,j]=rand.Next(10,99)+rand.NextDouble();
+            System.Console.Write($"{a[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+System.Console.WriteLine("Введите число строк:");
+int m=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число столбцов:");
+int n=Convert.ToInt32(Console.ReadLine());
+double[,] a=new double[m,n];
+Input(m,n,a);
